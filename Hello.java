34816066e7e5b04401060bdb910c9536e22cd1d1
Sampat <sampat@Sampats-MacBook-Air.local>
2026-01-20 01:03:28 +0530

    First commit

---
+++ Hello.java
@@ -0,0 +1,10 @@
+class Hello
+{
+    public static void main(String a[])
+    {
+        int num1 = 3;
+        System.out.println(num1*num1);
+        System.out.println(3+3);
+        System.out.println(3+4);
+    }
+}
