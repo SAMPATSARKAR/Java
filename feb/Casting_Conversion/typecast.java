02578decc3330d1e856991c11cdb910ed258fdcc
Sampat <sampat@Sampats-MacBook-Air.local>
2026-02-03 13:32:41 +0530

    Casting and consersion

---
+++ feb/Casting_Conversion/typecast.java
@@ -0,0 +1,13 @@
+class sol{
+    public static void main(String[] args){
+        // int a = 257;
+        // byte k = (byte) a;
+        // System.out.println(k);
+        // float f = 5.6f;
+        // int t = (int) f;
+
+        // System.out.println(t);
+        byte a = 10;
+        byte b = 30
+    }
+}
